a67aeca0be1de0b20c40611cc6ba3dbb6633e55f
Dipesh Chapagain <hancymig33@gmail.com>
2022-05-04 15:07:26 +0545

    Project Complete

--- Server/Controllers/DashboardController.cs
+++ Server/Controllers/DashboardController.cs
@@ -9,61 +9,29 @@ using HRAppMudBlazorProject.Server.Data;
 using HRAppMudBlazorProject.Server.Models;
 using HRAppMudBlazorProject.Shared;
 
-namespace HRAppMudBlazorProject.Server.Controllers
+namespace HRAppMudBlazorProject.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DashboardController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DashboardController : ControllerBase
+    private readonly ApplicationDbContext _context;
+
+    public DashboardController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-
-        public DashboardController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/<DashboardController>
-        [HttpGet]
-        public double[] Get()
-        {
-            var countMale = _context.Employee.Count(p => p.Sex == Models.Gender.Male);
-            var countFemale = _context.Employee.Count(p => p.Sex == Models.Gender.Female);
-            var countOthers = _context.Employee.Count(p => p.Sex == Models.Gender.Others);
-            double[] datas = {countMale,countFemale,countOthers};
-            
-            return datas;
-            
-        }
-
-        //[HttpGet("DataGet")]
-        //public async Task<ActionResult<IEnumerable<Department>>> DataGet()
-        //{
-        //    return await _context.Department.CountAsync().ToListAsync();
-        //}
-
-        // GET api/<DashboardController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<DashboardController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<DashboardController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<DashboardController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        _context = context;
     }
+
+    // GET: api/<DashboardController>
+    [HttpGet("GenderData")]
+    public double[] GenderDataGet()
+    {
+        var countMale = _context.Employee.Count(p => p.Sex == Models.Gender.Male);
+        var countFemale = _context.Employee.Count(p => p.Sex == Models.Gender.Female);
+        var countOthers = _context.Employee.Count(p => p.Sex == Models.Gender.Others);
+        double[] genderData = { countMale, countFemale, countOthers };
+        return genderData;
+
+    }
+
 }
